9b77c7d8c89b23cc8ae05dc6093fde29c052e23b
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 15:04:19 -0500

    added mothods

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,5 +1,25 @@
 class GradesController < ApplicationController
+
+  # GET /grades
+  # GET /grades.json
   def index
+    @grades = Grade.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @grades }
+    end
+  end
+
+  # GET /grades/1
+  # GET /grades/1.json
+  def show
+    @grade = Grade.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @grade }
+    end
   end
 
 end
