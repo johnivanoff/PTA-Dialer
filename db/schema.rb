8a41bf8b34efd1975491867caf31cfe0091d4cb8
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 15:54:35 -0500

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111004195451) do
+ActiveRecord::Schema.define(:version => 20111004205359) do
 
   create_table "grades", :force => true do |t|
     t.string   "name"
     t.integer  "display_order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "teacher_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
