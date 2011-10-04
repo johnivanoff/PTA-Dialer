5d7b2182c2b49e84328e1b64a96962d41d530bec
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 16:10:57 -0500

    methods for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,26 @@
 class StudentsController < ApplicationController
+
+  # GET /students
+  # GET /students.json
   def index
+    @students = Student.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @students }
+    end
   end
+
+  # GET /students/1
+  # GET /students/1.json
+  def show
+    @student = Student.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @student }
+    end
+  end
+
 
 end
