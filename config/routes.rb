87dbb2d85f0d2de248670c2809324d0ea4c3ce4c
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 15:21:25 -0500

    added a teacher route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pta::Application.routes.draw do
+  
+  get "teachers" => "teachers#index", :as => "teachers"
+
   get "grades" => "grades#index", :as => "grades"
   get "grades/:id" => "grades#show", :as => "grade"
 end
