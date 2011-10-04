813f083d62b6f90f38aad9283f2a3c1fc8db1cbf
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 11:51:50 -0500

    Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pta::Application.config.secret_token = '7af4bff25c9e57ae1ab486b6535116e9bf0e07d2551bd560981b36f30317450114e5b911f8311e0ab4a8e38764cd13ef8afe37c618ef33be323dd1220dd1b029'
