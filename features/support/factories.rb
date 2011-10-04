45d6b248100c1e966e3766e8907e54f932c3c902
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 15:03:53 -0500

    added teacher factory

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -2,3 +2,11 @@ Factory.define :grade do |f|
   f.name 'john@example.com'
   f.display_order 1
 end
+
+Factory.define :teacher do |f| 
+  f.first_name "Foo"
+  f.last_name "Bar"
+  f.email "foo.bar@example.com"
+  f.phone_number "972-555-1212"
+  f.grade_id 1
+end
