fcf383284d11295296d8612367f3da511cd7c665
John Ivanoff <john.ivanoff@gmail.com>
2011-10-04 16:11:25 -0500

    added relationships

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ActiveRecord::Base
-
+  
+  belongs_to :teacher
+  
   def whole_name 
     first_name + " " + last_name
   end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,7 @@
 class Teacher < ActiveRecord::Base
   
   belongs_to :grade
+  has_many :students
   
   def whole_name 
     first_name + " " + last_name
